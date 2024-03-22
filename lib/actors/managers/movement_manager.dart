154402535787cea45ba10195407abd4677efbb3a
Goru02 <ahmmednoaman5555@gmail.com>
2024-03-22 14:22:16 +0200

    Changed knockback velocity decay function

--- lib/actors/managers/movement_manager.dart
+++ lib/actors/managers/movement_manager.dart
@@ -29,7 +29,7 @@ abstract class MovementManager {
 
   void update(double dt) {
     _handleSpriteFlipByMovementDirection();
-    _handleExternalForces();
+    _handleExternalForces(dt);
   }
 
   void _handleSpriteFlipByMovementDirection() {
@@ -46,19 +46,19 @@ abstract class MovementManager {
 
   void addExponentiallyDecayingForce(Vector2 force) {
     externalForces.add(
-      CalculatedVector2((int counter) {
-        return force * exp(-counter * 0.5);
+      CalculatedVector2((double time) {
+        return force * exp(-time * 10);
       }),
     );
   }
 
-  void _handleExternalForces() {
+  void _handleExternalForces(double dt) {
     if (externalForces.isEmpty) return;
 
     Vector2 totalForce = Vector2.zero();
 
     for (final CalculatedVector2 force in externalForces) {
-      final Vector2 calculatedForce = force.getVector();
+      final Vector2 calculatedForce = force.getVector(dt);
       if (calculatedForce.length < 0.1) {
         force.kill();
         continue;
@@ -79,16 +79,16 @@ abstract class MovementManager {
 }
 
 class CalculatedVector2 {
-  final Vector2 Function(int) calculateVector;
-  int counter = -1;
+  final Vector2 Function(double) calculateVector;
+  double time = 0;
   bool killed = false;
 
   CalculatedVector2(this.calculateVector);
 
-  Vector2 getVector() {
+  Vector2 getVector(double dt) {
     if (killed) return Vector2.zero();
-    counter++;
-    return calculateVector(counter);
+    time += dt;
+    return calculateVector(time);
   }
 
   void kill() {
